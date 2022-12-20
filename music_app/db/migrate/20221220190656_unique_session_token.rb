9d9b03738c8053982f2eeb7f0d8ab97eccb63e8f
Wilson Wu <wuyecheng738@gmail.com>
2022-12-20 15:45:17 -0500

    completed application controller

--- music_app/db/migrate/20221220190656_unique_session_token.rb
+++ music_app/db/migrate/20221220190656_unique_session_token.rb
@@ -1,5 +1,5 @@
 class UniqueSessionToken < ActiveRecord::Migration[7.0]
   def change
-    change_column :users, :session_token, unique: true
+    change_column :users, :session_token, :string, unique: true
   end
 end
